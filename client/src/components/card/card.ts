433ec50ee723ed017845253e59e9ccc9f4afaefb
sooojungee <sooojungee@gmail.com>
2020-07-15 17:42:22 +0900

    card component 구현 - 마크업 - 삭제 기능 - 정보 보여주기

--- client/src/components/card/card.ts
+++ client/src/components/card/card.ts
@@ -1,14 +1,31 @@
+import Modal from '../modal';
+
 class Card extends HTMLElement{
-  private state : {} = {};
+  private state : {
+    title: string,
+    content: string,
+    user: string,
+  } = {
+    title: '',
+    content: '',
+    user: ''
+  };
+
+  private modal!: HTMLElement;
 
   constructor() {
     super();
+    this.modal = new Modal();
+    this.state.title='title입니다';
+    this.state.content='content입니다';
+    this.state.user='user';
   }
 
 
   connectedCallback() {
     // DOM에 추가되었다. 렌더링 등의 처리를 하자.
     this.render();
+    this.listener();
   }
 
   disconnectedCallback() {
@@ -18,10 +35,41 @@ class Card extends HTMLElement{
 
   attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
     this.render(); 
+    this.listener();
+  }
+
+  listener() {
+    const del = this.querySelector('.delete') as HTMLElement;
+    del.addEventListener('click', (e) => {
+      e.stopPropagation();
+      console.log('delete');
+      if (confirm('선택하신 카드를 삭제하시겠습니까?')) {
+        this.remove();
+      } 
+    });
+    this.addEventListener('dblclick', (e) => {
+      e.stopPropagation();
+      console.log('edit');
+      // this.modal.open(() => {
+      //  수정하는 창 열기
+      //  얘의 정보를 가져다 줘야 하나? 그렇다
+      // });
+    });
   }
 
   render() {
-    this.innerHTML = `<div>Card</div>`;
+    this.innerHTML = 
+    `<div class="card">
+      <!-- <div class="type-icon"></div> -->
+      <div class="card-contents">
+        <div class="title content text-ellipsis" >${this.state.title}</div>
+        <div class="card-content content">${this.state.content}</div>
+        <div class="card-user content text-ellipsis">by <span>${this.state.user}<span></div>
+      </div>
+      <div class="icon-content">
+        <i class="material-icons icon delete">delete_outline</i>
+      </div>
+    </div>`;
   }
 }
 
