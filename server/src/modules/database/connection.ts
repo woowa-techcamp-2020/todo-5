0d648622446c6c18a4171ac52b7a020471bf1d83
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-15 10:20:09 +0900

    add database config and open connection pool

---
+++ server/src/modules/database/connection.ts
@@ -0,0 +1,11 @@
+const database = {
+	database: 'kitty05',
+	user: 'todo05',
+	password: '1234',
+	host: '13.209.83.0',
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
+};
+
+export { database };
