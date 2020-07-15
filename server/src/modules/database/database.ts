5f7eb18c193c268ff202b1bc55b80b372e148b2a
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-15 17:31:32 +0900

    mysql 모듈화

--- server/src/modules/database/database.ts
+++
@@ -1,8 +0,0 @@
-import { database } from './connection';
-import mysql from 'mysql2/promise';
-
-const getPool = async () => {
-	return await mysql.createPool(database);
-};
-
-export default getPool;
