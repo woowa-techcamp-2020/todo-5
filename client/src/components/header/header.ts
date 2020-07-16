6790a97406cc9c27a19844aac4973a0c07a00f4e
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-16 17:35:29 +0900

    make header layout

--- client/src/components/header/header.ts
+++ client/src/components/header/header.ts
@@ -1,31 +1,33 @@
-class Header extends HTMLElement{
-  private state : {} = {};
+class Header extends HTMLElement {
+	private state: { title: string };
 
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+		this.state = { title: 'TODO 서비스' };
+	}
 
+	connectedCallback() {
+		// DOM에 추가되었다. 렌더링 등의 처리를 하자.
+		this.render();
+	}
 
-  connectedCallback() {
-    // DOM에 추가되었다. 렌더링 등의 처리를 하자.
-    this.render();
-  }
+	disconnectedCallback() {
+		// DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
+		this.remove();
+	}
 
-  disconnectedCallback() {
-    // DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
-    this.remove();
-  } 
+	attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
+		this.render();
+	}
 
-  attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
-    this.render(); 
-  }
-
-  render() {
-    this.innerHTML = `<header>Header</header>`;
-  }
+	render() {
+		this.innerHTML = `<header>
+      <div class="service-title">${this.state.title}</div>
+      <div class="service-menu">menu</div>
+    </header>`;
+	}
 }
 
-
-window.customElements.define("header-element", Header);
+window.customElements.define('header-element', Header);
 
 export default customElements.get('header-element');
