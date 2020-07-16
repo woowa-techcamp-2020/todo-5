77357a4c91af3edb238e82171cdc86e7ff26cbeb
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-16 09:45:06 +0900

    add api functions for content, column

--- client/src/components/column/column.ts
+++ client/src/components/column/column.ts
@@ -1,36 +1,93 @@
 import Card from '../card';
 
-class Column extends HTMLElement{
-  private state : {} = {};
-  private card!: HTMLElement;
-
-  constructor() {
-    super();
-    this.card = new Card();
-  }
-
-
-  connectedCallback() {
-    // DOM에 추가되었다. 렌더링 등의 처리를 하자.
-    this.render();
-    this.appendChild(this.card);
-  }
-
-  disconnectedCallback() {
-    // DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
-    this.remove();
-  } 
-
-  attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
-    this.render(); 
-  }
-
-  render() {
-    this.innerHTML = `<div class="column"></div>`;
-  }
+interface ColumnInterface {
+	column_id: number;
+	order_weight: number;
+	title: string;
 }
 
+class Column extends HTMLElement {
+	private state: {} = {};
+	private column_id!: number;
+	private order_weight!: number;
+	private column_title!: string;
+	private cards!: Array<HTMLElement>;
 
-window.customElements.define("column-element", Column);
+	constructor(data: ColumnInterface) {
+		super();
+		this.column_id = data.column_id;
+		this.order_weight = data.order_weight;
+		this.column_title = data.title;
+		this.cards = [];
+		this.getCards();
+	}
+
+	connectedCallback() {
+		// DOM에 추가되었다. 렌더링 등의 처리를 하자.
+		this.render();
+		const columnTag = this.querySelector('.column') as HTMLElement;
+		this.cards.map((card) => {
+			columnTag.appendChild(card);
+		});
+	}
+
+	disconnectedCallback() {
+		// DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
+		this.remove();
+	}
+
+	attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
+		this.render();
+	}
+
+	render() {
+		this.innerHTML = `<div class="column"></div>`;
+	}
+
+	private async getCards() {
+		const options = {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			// const response = await fetch(`${url}/api/cards`, options);
+			// const json = await response.json();
+
+			const dump = [
+				{
+					card_id: 1,
+					order_weight: 1,
+					title: '01010101',
+					content: 'contentcontent',
+					last_update: '12312312',
+					create_date: '123123123',
+				},
+				{
+					card_id: 2,
+					order_weight: 2,
+					title: '2222222',
+					content: 'contentcontent',
+					last_update: '12312312',
+					create_date: '123123123',
+				},
+				{
+					card_id: 3,
+					order_weight: 3,
+					title: '333333',
+					content: 'contentcontent',
+					last_update: '12312312',
+					create_date: '123123123',
+				},
+			];
+			await dump.forEach((card) => this.cards.push(new Card(card)));
+		} catch (err) {
+			console.log('Error getting documents', err);
+		}
+	}
+}
+
+window.customElements.define('column-element', Column);
 
 export default customElements.get('column-element');
