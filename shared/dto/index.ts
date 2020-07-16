b66f6d42cde5408db5ded9bf76c7bbe1a214d93e
sooojungee <sooojungee@gmail.com>
2020-07-16 17:22:15 +0900

    service dto 작성

--- shared/dto/index.ts
+++ shared/dto/index.ts
@@ -1,4 +1,5 @@
 import * as UserDTO from './user_dto';
 import * as CardDTO from './card_dto';
+import * as ServiceDTO from './service_dto';
 
-export { UserDTO, CardDTO };
+export { UserDTO, CardDTO, ServiceDTO };
