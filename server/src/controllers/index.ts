114e4ab30766e5d6858ce152be75b7bf86882196
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-16 13:42:53 +0900

    make user model, controller, route for user pai

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,3 @@
+import UserController from './user_controller';
+
+export { UserController };
---
+++ server/src/models/user.ts
@@ -0,0 +1,54 @@
+import { mysql } from '../modules/database/mysql';
+import { UserDTO } from '../../../shared/dto';
+import logger from '../config/logger';
+
+class User {
+	constructor() {}
+
+	/**
+	 * admin test용 전체 user 출력
+	 */
+	static async getUser() {
+		let userData: [];
+		try {
+			userData = await mysql.connect((con: any) => con.query('SELECT * FROM user'));
+		} catch (err) {
+			throw err;
+		}
+		return [...userData][0];
+	}
+
+	/**
+	 * user id로 user 출력
+	 * @param id
+	 */
+	static async getUserById(uid: string) {
+		let userData;
+		try {
+			userData = await mysql.connect((con: any) =>
+				con.query(`SELECT user_id, uid, name FROM user where user.uid = '${uid}'`)
+			);
+		} catch (err) {
+			throw err;
+		}
+		return [...userData][0][0];
+	}
+
+	/**
+	 * user 등록하는 메소드
+	 * @param user
+	 */
+	static async registerUser(user: UserDTO.CREATE) {
+		let userData;
+		try {
+			userData = await mysql.connect((con: any) => {
+				return con.query(`INSERT INTO user (uid, name) VALUES('${user.uid}', '${user.name}')`);
+			});
+			return user;
+		} catch (err) {
+			throw err;
+		}
+	}
+}
+
+export default User;
