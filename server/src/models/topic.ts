cac95f81bba4dc6aaa16254d92c00667310340fc
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-17 13:30:57 +0900

    convention auto changed

--- server/src/models/topic.ts
+++ server/src/models/topic.ts
@@ -2,53 +2,56 @@ import { mysql } from '../modules/database/mysql';
 import { TopicDTO } from '../../../shared/dto';
 
 class Topic {
-  constructor() {}
-  static async create(topic: TopicDTO.CREATE) {
-    try {
-      const topicData = await mysql.connect((con: any) => {
-        return con.query(`INSERT INTO topic (order_weight, service_id, topic_title, removed) VALUES ('${topic.order_weight}', '${topic.service_id}', '${topic.topic_title}', '${topic.removed}')`)
-      });
-      return topic;
-    } catch (err) {
-      throw err;
-    }
-  }
+	constructor() {}
+	static async create(topic: TopicDTO.CREATE) {
+		try {
+			const topicData = await mysql.connect((con: any) => {
+				return con.query(
+					`INSERT INTO topic (order_weight, service_id, topic_title, removed) VALUES ('${topic.order_weight}', '${topic.service_id}', '${topic.topic_title}', '${topic.removed}')`
+				);
+			});
+			return topic;
+		} catch (err) {
+			throw err;
+		}
+	}
 
-  static async update(topic: TopicDTO.UPDATE) {
-    try {
-      const topicData = await mysql.connect((con: any) => {
-        return con.query(`UPDATE topic SET order_weight = '${topic.order_weight}', topic_title = '${topic.topic_title}' WHERE topic_id = '${topic.topic_id}'`)
-      });
-      return topic;
-    } catch (err) {
-      throw err;
-    }
-  }
+	static async update(topic: TopicDTO.UPDATE) {
+		try {
+			const topicData = await mysql.connect((con: any) => {
+				return con.query(
+					`UPDATE topic SET order_weight = '${topic.order_weight}', topic_title = '${topic.topic_title}' WHERE topic_id = '${topic.topic_id}'`
+				);
+			});
+			return topic;
+		} catch (err) {
+			throw err;
+		}
+	}
 
-  static async delete(topic: TopicDTO.UPDATE) {
-    try {
-      const topicData = await mysql.connect((con: any) => {
-        return con.query(`UPDATE topic SET removed = '${1}' WHERE topic_id = '${topic.topic_id}'`)
-      });
-      return topic;
-    } catch (err) {
-      throw err;
-    }
-  }
+	static async delete(topic: TopicDTO.UPDATE) {
+		try {
+			const topicData = await mysql.connect((con: any) => {
+				return con.query(`UPDATE topic SET removed = '${1}' WHERE topic_id = '${topic.topic_id}'`);
+			});
+			return topic;
+		} catch (err) {
+			throw err;
+		}
+	}
 
-  static async getTopicsByServiceId(serviceId: string) {
-    // 필요하지 remove false, column_id보고 데려오면 되는건가?
-    let topicData;
+	static async getTopicsByServiceId(serviceId: string) {
+		// 필요하지 remove false, topic_id보고 데려오면 되는건가?
+		let topicData;
 		try {
 			topicData = await mysql.connect((con: any) =>
 				con.query(`SELECT * FROM topic WHERE service_id = '${serviceId}'`)
-      );
-      return [...topicData][0];
+			);
+			return [...topicData][0];
 		} catch (err) {
 			throw err;
-    }
-
-  }
+		}
+	}
 }
 
 export default Topic;
