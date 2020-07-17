ad4b3eda9c196bd21d602e60a73624ec530d688c
sooojungee <sooojungee@gmail.com>
2020-07-17 10:56:58 +0900

    card api 작성

--- server/src/controllers/card_controller.ts
+++ server/src/controllers/card_controller.ts
@@ -18,6 +18,48 @@ class CardController {
       next(err);
     }
    }
+
+   static async getCardsByTopicId(req: Request, res: Response, next: NextFunction) {
+    let response;
+    const topicId = req.params.topic_id;
+    try {
+      response = await Card.getCardsBytopicId(topicId);
+      if (response.result === null) {
+        new Error('no topic exists');
+      }
+      res.status(httpStatus.OK)
+      .json(JsonResponse(httpStatus.OK, 'cards get well', response));
+    } catch(err) {
+      logger.error('cards get fail', err);
+      next(err);
+    }
+  }
+  
+  static async update(req: Request, res: Response, next: NextFunction) {
+    let response;
+    let { body } = req;
+    try {
+      response = await Card.update(body);
+      res.status(httpStatus.OK)
+      .json(JsonResponse(httpStatus.OK, 'card updated well', response));
+    } catch(err) {
+      logger.error('card updated fail', err);
+      next(err);
+    }
+  }
+
+  static async delete(req: Request, res: Response, next: NextFunction) {
+    let response;
+    let { body } = req;
+    try {
+      response = await Card.delete(body);
+      res.status(httpStatus.OK)
+      .json(JsonResponse(httpStatus.OK, 'card deleted well', response));
+    } catch(err) {
+      logger.error('card deleted fail', err);
+      next(err);
+    }
+  }
 }
 
 export default CardController;
--- server/src/routes/card_router.ts
+++ server/src/routes/card_router.ts
@@ -3,5 +3,8 @@ import { CardController } from '../controllers';
 
 const cardRouter = Router();
 cardRouter.route('/').post(CardController.create);
+cardRouter.route('/update').post(CardController.update);
+cardRouter.route('/delete').post(CardController.delete);
+cardRouter.route('/:topic_id').get(CardController.getCardsByTopicId);
 
-export default cardRouter;
+export default cardRouter;     
