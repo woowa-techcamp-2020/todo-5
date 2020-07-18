8d12841409597f40567e6ae7ace4085c3e872776
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-18 23:17:15 +0900

    setting logger according to the environment

--- server/src/config/logger.ts
+++ server/src/config/logger.ts
@@ -1,13 +1,39 @@
 import winston from 'winston';
+import { env } from './vars';
+import { info } from 'console';
+const winstonDaily = require('winston-daily-rotate-file');
+
+const timeStampFormant = () => Date().toLocaleLowerCase();
 
 const logger = winston.createLogger({
-	level: 'info',
-	format: winston.format.json(),
 	transports: [
-		new winston.transports.Console({
-			format: winston.format.simple(),
+		new winstonDaily({
+			name: 'error-file',
+			filename: 'logs/error',
+			level: 'error',
+			datePattern: 'yyyy-MM-dd.log',
+			timestamp: timeStampFormant(),
+			json: false,
+			prepend: true,
+		}),
+		new winstonDaily({
+			name: 'combined-file',
+			filename: 'logs/combined',
+			datePattern: 'yyyy-MM-dd.log',
+			timestamp: timeStampFormant(),
+			json: false,
+			prepend: true,
 		}),
 	],
 });
+
+if (env !== 'production') {
+	logger.add(
+		new winston.transports.Console({
+			level: 'info',
+			format: winston.format.simple(),
+		})
+	);
+}
 
 export default logger;
