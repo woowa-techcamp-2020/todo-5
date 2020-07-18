2fb49efe79113c1155e2b24c7188c9d29d8491ad
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-18 23:33:08 +0900

    webpack config production과 development 모드 설정했습니다.

---
+++ client/webpack-dev.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+	mode: 'development',
+	entry: './src/app.ts',
+	output: {
+		path: path.resolve(__dirname, '../server/src/public/dist'),
+		filename: '[name].js',
+	},
+	devtool: 'cheap-eval-source-map',
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpg)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.ts$/,
+				use: ['ts-loader'],
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					require.resolve('style-loader'),
+					{
+						loader: require.resolve('css-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('sass-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(css)$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
---
+++ client/webpack-pod.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	mode: 'production',
+	entry: './src/app.ts',
+	output: {
+		path: path.resolve(__dirname, '../server/src/public/dist'),
+		filename: '[name].js',
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpg)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.ts$/,
+				use: ['ts-loader'],
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					require.resolve('style-loader'),
+					{
+						loader: require.resolve('css-loader'),
+					},
+					{
+						loader: require.resolve('sass-loader'),
+					},
+				],
+			},
+			{
+				test: /\.(css)$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
