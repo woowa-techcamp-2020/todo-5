d4b0421e5dce5ce1286c398f0167bb14fb4082e8
sooojungee <sooojungee@gmail.com>
2020-07-21 11:23:04 +0900

    Drag and drop 구현
    clone을 통해 마우스를 따라 움직이는 card를 만들고, 해당 좌표의 topic과 card를 찾은 후, 추가하는 방식을 사용하였습니다.
    
    애니메이션을 추가해야 합니다

--- client/src/pages/todo-list/todo-list.ts
+++ client/src/pages/todo-list/todo-list.ts
@@ -18,10 +18,10 @@ class TodoList extends HTMLElement {
 	connectedCallback() {
 		// DOM에 추가되었다. 렌더링 등의 처리를 하자.
 		this.render();
-		console.log('render');
 		this.appendChild(this.sidebar);
 		this.appendChild(this.header);
 		this.appendChild(this.content);
+		this.mouseEvent();
 	}
 
 	disconnectedCallback() {
@@ -31,6 +31,109 @@ class TodoList extends HTMLElement {
 
 	attributeChangedCallback(attrName: any, oldVal: any, newVal: any) {
 		this.render();
+	}
+
+	private card = {
+		mouseDownX: 0,
+		mouseDownY: 0,
+		isDragging: false,
+		clicked: false,
+		selected: document.createElement('div') as HTMLElement,
+		moving: document.createElement('div'),
+		cloned: document.createElement('div'),
+	};
+	// private mose
+
+	mouseEvent() {
+		document.addEventListener('mousedown', (event: MouseEvent) => this.mouseDown(event));
+		document.addEventListener('mousemove', (event: MouseEvent) => this.mouseMove(event));
+		document.addEventListener('mouseup', (event: MouseEvent) => this.mouseUp(event));
+		document.addEventListener('mouseleave', (event: MouseEvent) => this.mouseLeave(event));
+		this.initMovingCardWrapper();
+	}
+
+	initMovingCardWrapper() {
+		this.card.moving.classList.add('moving');
+		this.appendChild(this.card.moving);
+	}
+
+	mouseDown(event: MouseEvent) {
+		this.card.mouseDownX = event.pageX;
+		this.card.mouseDownY = event.pageY;
+		this.card.clicked = true;
+	}
+
+	mouseMove(event: MouseEvent) {
+		if (!this.card.clicked) return;
+		if (
+			(this.card.mouseDownX - event.pageX !== 0 || this.card.mouseDownY - event.pageY !== 0) &&
+			!this.card.isDragging
+		) {
+			const card = (event.target as HTMLElement).closest('card-element') as HTMLElement;
+			if (!card) return;
+			this.card.isDragging = true;
+			// @TODO
+			// @ts-ignore
+			const moving = card.clone();
+			// @ts-ignore
+			this.card.cloned = card;
+			this.card.cloned.classList.add('cloned');
+			this.card.moving.appendChild(moving);
+		}
+		if (this.card.isDragging) {
+			this.card.moving.style.left = event.pageX - this.card.moving.offsetWidth / 2 + 'px';
+			this.card.moving.style.top = event.pageY - this.card.moving.offsetHeight / 2 + 'px';
+
+			this.card.moving.hidden = true;
+			const topic = document
+				.elementFromPoint(event.pageX, event.pageY)
+				?.closest('topic-element')
+				?.querySelector('.topic-content') as HTMLElement;
+			const card = document
+				.elementFromPoint(event.pageX, event.pageY)
+				?.closest('card-element') as HTMLElement;
+			this.card.moving.hidden = false;
+
+			if (!topic || card?.classList.contains('cloned')) return;
+			if (!card) {
+				if (!topic.querySelector('.cloned')) {
+					const top = topic.getBoundingClientRect().top;
+					if (event.pageY < top && topic.querySelector('card-element')) {
+						topic.insertBefore(this.card.cloned, topic.childNodes[1]);
+					} else {
+						topic.appendChild(this.card.cloned);
+					}
+				}
+			} else {
+				const rect = card.getBoundingClientRect().top;
+				const top = parseInt(this.card.moving.style.top.split('px')[0]);
+				if (top > rect) {
+					if ((card.nextSibling as HTMLElement).classList.contains('cloned')) return;
+					card.parentNode?.insertBefore(this.card.cloned, card.nextSibling);
+				} else {
+					if (this.card.cloned.nextSibling === card) return;
+					card.parentNode?.insertBefore(this.card.cloned, card);
+				}
+			}
+		}
+	}
+
+	mouseUp(event: MouseEvent) {
+		this.card.clicked = false;
+		this.card.isDragging = false;
+		if (this.card.moving.hasChildNodes()) {
+			this.card.moving.removeChild(this.card.moving.firstChild as ChildNode);
+		}
+		//
+		this.card.cloned.classList.remove('cloned');
+		this.card.selected.remove();
+		//
+	}
+
+	mouseLeave(event: MouseEvent) {
+		this.card.clicked = false;
+		this.card.isDragging = false;
+		this.mouseUp(event);
 	}
 
 	render() {
