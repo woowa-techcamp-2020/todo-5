d0d8027113c99f765efcef973ce7705a16059c86
sooojungee <sooojungee@gmail.com>
2020-07-22 12:53:08 +0900

    Card input 스타일 수정
    기존의 <button> 태그를 <div> 태그로 변경하여 디자인함
    색상 변경,
    input창 생성 시 버튼 초기화
    input창 수정

--- client/src/components/card-input/card-input.ts
+++ client/src/components/card-input/card-input.ts
@@ -29,21 +29,16 @@ class CardInput extends HTMLElement {
 		this.innerHTML = `<div class="card-input">
             <textarea placeholder="Enter a card"></textarea>
             <div class="btn-group">
-                <button class="card-add">Add</button>
-                <button class="card-close">Cancel</button>
+                <div class="button card-add">Add</div>
+                <div class="button card-close">Cancel</div>
             </div>
         </div>`;
 	}
 
 	listener() {
 		const inputBox = this.querySelector('textarea') as HTMLTextAreaElement;
-		const btnGroup = this.querySelectorAll('button');
+		const btnGroup = this.querySelectorAll('.button');
 		inputBox.addEventListener('input', (e) => {
-			if (inputBox.textLength > 200) {
-				//글자 수 제한
-				inputBox.value = inputBox.value.substring(0, 200);
-			}
-
 			if (inputBox.value === '') {
 				//add 버튼 활성화
 				btnGroup[0].classList.remove('enable-btn');
@@ -55,18 +50,33 @@ class CardInput extends HTMLElement {
 			this.resolve({
 				user_id: 1,
 				user_name: 'loloara',
-				content: inputBox?.value,
+				content: inputBox.value,
 				topic_id: 1,
 				card_title: 'title',
 				order_weight: 1,
 			});
 			this.reject();
-			inputBox.value = '';
+			this.resetInput(inputBox, btnGroup);
+			if (inputBox.textLength > 200) {
+				//글자 수 제한
+				inputBox.value = inputBox.value.substring(0, 200);
+			}
 		});
 		btnGroup[1].addEventListener('click', (e) => {
 			this.reject();
-			inputBox.value = '';
+			this.resetInput(inputBox, btnGroup);
+			if (inputBox.textLength > 200) {
+				//글자 수 제한
+				inputBox.value = inputBox.value.substring(0, 200);
+			}
 		});
+	}
+
+	resetInput(inputBox: HTMLTextAreaElement, btnGroup: NodeListOf<Element>) {
+		inputBox.value = '';
+		if (btnGroup[0].classList.contains('enable-btn')) {
+			btnGroup[0].classList.remove('enable-btn');
+		}
 	}
 	openCardInput() {
 		const cardInput = this.querySelector('.card-input');
