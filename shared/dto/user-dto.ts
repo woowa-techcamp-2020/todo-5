e22c8fef889c94a123d7ff15c9c3de29dc0e8961
Jong-ku Lee <addy94@naver.com>
2020-07-23 14:15:05 +0900

    Update user-dto.ts

--- shared/dto/user-dto.ts
+++ shared/dto/user-dto.ts
@@ -13,12 +13,3 @@ interface RESPONSE {
 
 export { CREATE, RESPONSE };
 
-card_id: 106;
-card_title: '123123123123';
-content: '';
-create_date: 1595415792;
-last_update: 1595415792;
-order_weight: 900000;
-topic_id: 41;
-uid: 'loloara';
-user_id: 1;
