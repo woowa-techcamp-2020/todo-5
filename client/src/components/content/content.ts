d9a84e47a108159a65147497259e46b1fd5b9fbe
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-23 21:12:47 +0900

    add getTopicTitleFromId and map data structure

--- client/src/components/content/content.ts
+++ client/src/components/content/content.ts
@@ -1,7 +1,8 @@
 import Topic from '../topic';
-import { ORDER_WEIGHT, DUMMY_USER } from '../../api/utils';
+import { ORDER_WEIGHT } from '../../utils';
 import { $inputTextModal } from '../modal';
 import { TopicApi, ActivityApi } from '../../api';
+import store from '../../store';
 
 interface ContentInterface {
 	service_id: string;
@@ -10,18 +11,23 @@ interface ContentInterface {
 class Content extends HTMLElement {
 	private state: ContentInterface;
 	private topics!: Array<typeof Topic>;
+	private topicsMap: Map<number, string>;
 
 	constructor(data: ContentInterface) {
 		super();
 		this.state = data;
 		this.topics = [];
+		this.topicsMap = new Map();
 	}
 
 	async connectedCallback() {
 		await this.getTopics();
 		this.render();
 		const contentTag = this.querySelector('.content') as HTMLElement;
-		this.topics.forEach((topic: typeof Topic) => contentTag.appendChild(topic));
+		this.topics.forEach((topic: typeof Topic) => {
+			contentTag.appendChild(topic);
+			this.topicsMap.set(topic.getTopicId(), topic.getTopicTitle());
+		});
 	}
 
 	disconnectedCallback() {
@@ -67,7 +73,7 @@ class Content extends HTMLElement {
 			service_id: parseInt(this.state.service_id),
 			topic_title: topic_title,
 			order_weight: nextOrderWeight,
-			user_id: DUMMY_USER,
+			user_id: store.getState('user_id'),
 		};
 
 		try {
@@ -77,6 +83,7 @@ class Content extends HTMLElement {
 			const newTopic = new Topic(res.result);
 			contentTag.appendChild(newTopic);
 			this.topics.push(newTopic);
+			this.topicsMap.set(newTopic.getTopicId(), newTopic.getTopicTitle());
 		} catch (e) {
 			alert('카드 추가에 실패하였습니다.');
 		}
@@ -87,10 +94,16 @@ class Content extends HTMLElement {
 			const response = await TopicApi.getAll(this.state.service_id);
 			const sortedTopics = [...response.result];
 			sortedTopics.sort((a: typeof Topic, b: typeof Topic) => a.order_weight - b.order_weight);
-			await sortedTopics.forEach((topic) => this.topics.push(new Topic(topic)));
+			sortedTopics.forEach((topic) => this.topics.push(new Topic(topic)));
 		} catch (err) {
 			console.error('Error getting documents', err);
 		}
+	}
+
+	public getTopicTitleFromId(topic_id: number): string {
+		return this.topicsMap.has(topic_id)
+			? (this.topicsMap.get(topic_id) as string)
+			: 'No Title Topic';
 	}
 }
 
