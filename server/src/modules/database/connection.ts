b35c60f2921bac5118781a0a897265574dfccead
Ubuntu <ubuntu@ip-172-31-36-135.ap-northeast-2.compute.internal>
2020-09-22 10:59:12 +0000

    chore: fix typo (PORD_DB -> PROD_DB)

--- server/src/modules/database/connection.ts
+++ server/src/modules/database/connection.ts
@@ -1,7 +1,7 @@
 import { env } from '../../config/vars';
 
 const database = {
-	database: env === 'production' ? process.env.PORD_DB : process.env.DEV_DB,
+	database: env === 'production' ? process.env.PROD_DB : process.env.DEV_DB,
 	user: process.env.DB_USER,
 	password: process.env.DB_PW,
 	host: process.env.DB_HOST,
