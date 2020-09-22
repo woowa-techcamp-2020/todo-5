12f1ba45f98da0ca29c91fa8821f258b0536cd48
loloara <addy94@naver.com>
2020-09-22 17:55:47 +0900

    deploy: ts-node를 이용한 실행 스크립트 작성

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'todo-service',
-			script: 'node ./node_modules/ts-node/dist/bin.js -r tsconfig-paths/register ./src/main.ts',
+			script: 'node ./node_modules/ts-node/dist/bin.js ./src/main.ts',
 			watch: false,
 			out_file: './logs/out.log',
 			error_file: './logs/error.log',
